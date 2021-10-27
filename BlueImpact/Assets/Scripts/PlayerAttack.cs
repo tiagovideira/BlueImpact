d42f1367a5e7cac2d24ca82b0723cd493e5793a6
tiagovideira <tiago.f.m.v1@gmail.com>
2021-10-27 12:48:38 +0100

    Player Kick and Dash implemented

--- BlueImpact/Assets/Scripts/PlayerAttack.cs
+++ BlueImpact/Assets/Scripts/PlayerAttack.cs
@@ -4,22 +4,35 @@ public class PlayerAttack : MonoBehaviour
 {
 
     public float PunchDamage;
-    public EnemyDetection EnemyDetection;
+    public float KickDamage;
+
+    public EnemyDetection PunchRange;
+    public EnemyDetection KickRange;
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-
             Punch(PunchDamage);
         }
-
+        if (Input.GetMouseButtonDown(1))
+        {
+            Kick(KickDamage);
+        }
     }
 
     private void Punch(float PunchDamage)
     {
-        foreach (GameObject Enemy in EnemyDetection.EnemyList)
+        foreach (GameObject Enemy in PunchRange.EnemyList)
         {
             Enemy.GetComponent<EnemyHealth>().TakeDamage(PunchDamage);
+        }
+    }
+
+    private void Kick(float KickDamage)
+    {
+        foreach (GameObject Enemy in KickRange.EnemyList)
+        {
+            Enemy.GetComponent<EnemyHealth>().TakeDamage(KickDamage);
         }
     }
 }
--- BlueImpact/Assets/Scripts/PlayerMovement.cs
+++ BlueImpact/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,20 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float moveSpeed;
     [SerializeField]
+    private float dashDistance;
+    private float lastTapTime = 0;
+    private float dashTime = 0.5f;
+    private float dashCooldown;
     public Rigidbody2D rb2d;
-    private Vector2 moveDirection;
+    private Vector3 moveDirection;
     public float moveX;
     public float moveY;
     private bool facingRight;
+
+    void Start()
+    {
+        lastTapTime = 0;
+    }
 
     void FixedUpdate()
     {
@@ -20,6 +29,33 @@ public class PlayerMovement : MonoBehaviour
     {
         GetInput();
         Flip();
+
+
+        // Programação Dash
+        dashCooldown += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.D) && dashCooldown >= 1)
+        {
+
+            if (Input.GetKeyDown(KeyCode.D) && (Time.time - lastTapTime) < dashTime)
+            {
+                Dash("Right");
+            }
+
+            lastTapTime = Time.time;
+
+        }
+        if (Input.GetKeyDown(KeyCode.A) && dashCooldown >= 1)
+        {
+
+            if (Input.GetKeyDown(KeyCode.A) && (Time.time - lastTapTime) < dashTime)
+            {
+                Dash("Left");
+            }
+
+            lastTapTime = Time.time;
+
+        }
     }
 
     private void GetInput()
@@ -27,20 +63,35 @@ public class PlayerMovement : MonoBehaviour
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
 
-        moveDirection = new Vector2(moveX, moveY);
+
+        moveDirection = new Vector3(moveX, moveY).normalized;
+
     }
 
     private void Movement()
     {
-        rb2d.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        rb2d.velocity = new Vector3(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    private void Dash(string Direction)
+    {
+        if (Direction == "Right")
+        {
+            rb2d.MovePosition(transform.position + new Vector3(1, 0) * dashDistance);
+        }
+        if (Direction == "Left")
+        {
+            rb2d.MovePosition(transform.position + new Vector3(-1, 0) * dashDistance);
+        }
+        dashCooldown = 0;
     }
 
     private void Flip()
     {
-        if(moveX < 0 && !facingRight || moveX > 0 && facingRight)
+        if (moveX < 0 && !facingRight || moveX > 0 && facingRight)
         {
             facingRight = !facingRight;
-            
+
             Vector3 scale = transform.localScale;
             scale.x *= -1;
             transform.localScale = scale;
