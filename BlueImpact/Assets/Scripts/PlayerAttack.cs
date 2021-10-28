9f918a667aad870159c3784f92c36be028adfbac
tiagovideira <tiago.f.m.v1@gmail.com>
2021-10-28 11:34:34 +0100

    Combo System(Com bugs)

--- BlueImpact/Assets/Scripts/PlayerAttack.cs
+++ BlueImpact/Assets/Scripts/PlayerAttack.cs
@@ -6,30 +6,79 @@ public class PlayerAttack : MonoBehaviour
     public float PunchDamage;
     public float KickDamage;
 
+    public float PunchDelay;
+    public float KickDelay;
+
     public EnemyDetection PunchRange;
     public EnemyDetection KickRange;
+
+    public float AttackDelta = 0;
+    public float ComboDeltaTime;
+    public bool InCombo = false;
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        AttackDelta += Time.deltaTime;
+
+        if (AttackDelta > ComboDeltaTime)
         {
-            Punch(PunchDamage);
+            InCombo = false;
         }
-        if (Input.GetMouseButtonDown(1))
+
+        if (Input.GetMouseButtonDown(0))//Punch
         {
-            Kick(KickDamage);
+            Invoke("Punch", PunchDelay);
+            Debug.Log("Punch");
+            InCombo = true;
+
+            if (Input.GetMouseButtonDown(0) && AttackDelta <= ComboDeltaTime)//Punch Punch
+            {
+                Invoke("Punch", PunchDelay);
+                Debug.Log("Punch Punch");
+
+                if (Input.GetMouseButtonDown(0) && AttackDelta <= ComboDeltaTime)//Punch Punch Punch
+                {
+                    //QuickAttack
+                    Debug.Log("QuickAttack");
+                    if (Input.GetMouseButtonDown(1) && AttackDelta <= ComboDeltaTime)//Punch Punch Punch Kick
+                    {
+                        //Special Combo 3
+                        Debug.Log("Combo 3");
+                    }
+                }
+                else if (Input.GetMouseButtonDown(1) && AttackDelta <= ComboDeltaTime)//Punch Punch Kick
+                {
+                    //Special Combo 2
+                    Debug.Log("Combo 2");
+                }
+
+            }
+            else if (Input.GetMouseButtonDown(1) && AttackDelta <= ComboDeltaTime)//Punch Kick
+            {
+                //Special Combo 1
+                Debug.Log("Combo 1");
+            }
+
+        }
+
+        if (Input.GetMouseButtonDown(1))//Pontapé básico
+        {
+            Invoke("Kick", KickDelay);
+            InCombo = true;
         }
     }
 
-    private void Punch(float PunchDamage)
+    private void Punch()
     {
+        AttackDelta = 0;
         foreach (GameObject Enemy in PunchRange.EnemyList)
         {
             Enemy.GetComponent<EnemyHealth>().TakeDamage(PunchDamage);
         }
     }
 
-    private void Kick(float KickDamage)
+    private void Kick()
     {
+        AttackDelta = 0;
         foreach (GameObject Enemy in KickRange.EnemyList)
         {
             Enemy.GetComponent<EnemyHealth>().TakeDamage(KickDamage);
