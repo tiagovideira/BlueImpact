2f9a1373a7a09d91174e03eb34e8b5fe28414bbf
tiagovideira <tiago.f.m.v1@gmail.com>
2022-01-04 01:28:54 +0000

    New Levels Imported and rebuilded, Level Ending condition and UI

--- BlueImpact/Assets/Scripts/LevelEnd.cs
+++ BlueImpact/Assets/Scripts/LevelEnd.cs
@@ -8,22 +8,52 @@ public class LevelEnd : MonoBehaviour
 {
     [SerializeField]
     private Image endingPanel;
-    private bool endLevel;
+    private bool endLevel = false;
+
+    [SerializeField]
+    private GameObject enemies;
+    private int enemyCount;
+
+    [SerializeField]
+    private GameObject cantEndUI;
+    [SerializeField]
+    private GameObject canEndUI;
+
+    private bool levelCanEnd;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        endLevel = true;
+        //endLevel = true;
+        enemyCount = enemies.transform.childCount;
+
+        if (enemyCount == 0)
+        {
+            levelCanEnd = true;
+            canEndUI.SetActive(true);
+        }
+        else
+        {
+            levelCanEnd = false;
+            cantEndUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        canEndUI.SetActive(false);
+        cantEndUI.SetActive(false);
     }
 
     private void Update()
     {
-        if(endLevel)
+        if (endLevel)
         {
-            endingPanel.color = new Color(endingPanel.color.r, endingPanel.color.g, endingPanel.color.b, endingPanel.color.a + Time.deltaTime);
-            StartCoroutine("LoadMenu");
+            //endingPanel.color = new Color(endingPanel.color.r, endingPanel.color.g, endingPanel.color.b, endingPanel.color.a + Time.deltaTime);
+            StartCoroutine("LoadNextLevel");
         }
     }
 
-    public IEnumerator LoadMenu()
+    public IEnumerator LoadNextLevel()
     {
         yield return new WaitForSeconds(2);
         SceneManager.LoadScene(0);
