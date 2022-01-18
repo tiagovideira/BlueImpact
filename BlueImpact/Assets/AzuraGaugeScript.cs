3318a11ae3eecc36642b161349c0041c0d5bb915
tiagovideira <tiago.f.m.v1@gmail.com>
2022-01-18 17:56:15 +0000

    Azura Gauge visual working

--- BlueImpact/Assets/AzuraGaugeScript.cs
+++ BlueImpact/Assets/AzuraGaugeScript.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AzuraGaugeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Image azuraGauge;
+    public float CurrentEnergy;
+    private float maxEnergy = 100f;
+    [SerializeField]
+    private PlayerAttack playerAttackScript;
+    private void Start()
     {
-        
+        azuraGauge = this.GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        CurrentEnergy = playerAttackScript.Energy;
+        azuraGauge.fillAmount = CurrentEnergy / maxEnergy;
     }
+
 }
